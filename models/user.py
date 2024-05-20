db52790edc7bc15760195104347bdc4b8d85bc3c
Waithira <dorotttyyy@gmail.com>
2024-05-20 14:08:49 -0700

    airbnb project

--- models/user.py
+++ models/user.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
+"""This defines the classe user module """
 from models.base_model import BaseModel
 
-class UserProfile(BaseModel):
-    """Defines a UserProfile object.
-
-    This class represents a user's profile information, including their email,
-    password, first name, and last name.
-
-    Attributes:
-        email (str): The email address associated with the user's account.
-        password (str): The hashed password for the user's account.
-        first_name (str): The user's first name.
-        last_name (str): The user's last name.
-    """
-
-    email_address = ""
-    hashed_password = ""
-    given_name = ""
-    family_name = "
+class User(BaseModel):
+    """This is the class defining various attributes of a user"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
