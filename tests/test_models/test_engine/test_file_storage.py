9bb217cd0a0a540ac9e8af2cc1962ebbe319dad4
Waithira <dorotttyyy@gmail.com>
2024-05-20 14:59:06 -0700

    airbnb projects

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" the module to use for testing the file storage"""
+import unittest
+
+class test_fileStorage(unittest.TestCase):
+    """ the class to use to test file storage methods """
+
+    def test_save(self):
+        """ docsting """
+        pass
