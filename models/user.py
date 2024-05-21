56a4dbcef6a7b9c1ea10f19f380161b076a20e1c
Waithira <dorotttyyy@gmail.com>
2024-05-21 15:07:32 -0700

    HBNB Project

--- models/user.py
+++ models/user.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-"""This defines the classe user module """
-from models.base_model import BaseModel
-
-class User(BaseModel):
-    """This is the class defining various attributes of a user"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
-
-
-#!/usr/bin/python3
 """location interpreter"""
 from models.base_model import BaseModel
 """import models"""
