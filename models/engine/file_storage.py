68291a25320065d57a5c6ac690e12a4b3bf486cd
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-05 22:05:39 +0300

    Hbnbclone
    clone attempt

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,48 +1,73 @@
 #!/usr/bin/python3
-"""defining the location interpreter"""
+#!/usr/bin/env python3
+""" The Class FileStorage that serialize the instances to a JSON file
+    and deserialize the JSON file to an instance """
+
 import json
+from os import read
+from models.base_model import BaseModel
+from models.city import City
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 import os.path
-from ..base_model import BaseModel
-from ..user import User
-from ..state import State
-from ..place import Place
-from ..amenity import Amenity
-from ..city import City
-from ..review import Review
-"""import models"""
+
 
 class FileStorage:
-    """This is serializing instances to a JSON file
-    and deserializes JSON file to instances"""
-    __file_path = "file.json"
-    __objects = {}
-    """the private class attributes"""
+    """ 
+    The class FileStorage to seriliaze and deserialize the instances to JSON
+        __file_path: the path of the json file
+        __objects: a dictionnary of all objects"
+    """
+    def __init__(self):
+        """ initializing the FileStorage
+        """
+        self.__file_path = "file.json"
+        self.__objects = {}
 
     def all(self):
-        """this is the public instance attributes or the method to return dictionary __objects"""
-        return FileStorage.__objects
+        """ Will return the dictionary of all the objects """
+        return self.__objects
 
     def new(self, obj):
-        """The public instance sets in __objects
-        the obj with mykey <obj class name>.id"""
-        mykey = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[mykey] = obj
+        """ To set in __objects the object (obj) with the key <obj class name>.id """
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
 
     def save(self):
-        """the public instance method to serialize
-        __objects to JSON file"""
-        dict_temp = {}
-        for mykey, value in FileStorage.__objects.items():
-            dict_temp[mykey] = value.to_dict()
-            with open(FileStorage.__file_path, "w") as f:
-                f.write(json.dumps(dict_temp))
+        """serialize the  __objects to JSON file (path: __file_path)
+            dictionary: empty dictionary
+                Open dictionary in write mode
+                dump dictionary in the file fl
+        """
+        dictionary = {}
+        with open(self.__file_path, 'w') as fl:
+            for obj in self.__objects.values():
+                key = obj.__class__.__name__ + "." + obj.id
+                dictionary[key] = obj.to_dict()
+            json.dump(dictionary, fl)
 
     def reload(self):
-        """it deserializes
-        the JSON file to __objects"""
-        if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r") as f:
-                dict_temp = json.loads(f.read())
-                for mykey, value in dict_temp.items():
-                    obj = eval(value['__class__'])(**value)
-                    FileStorage.__objects[mykey] = obj
+        """deserialize JSON file to __objects
+        (only if JSON file (__file_path) exists
+        otherwise, do nothing.
+        If file, fl is non-existent, no exception should be raised)
+            Opening in read mode"
+            loading fl and reading it
+            """
+        try:
+            with open(self.__file_path, 'r') as fl:
+                my_dict = json.load(fl)
+            for key, value in my_dict.items():
+                """the for loop will utilise the key value pair to run items in my_dict (my_dict.items())
+                creating a dictionary of key and value"""
+                new_obj = key.split('.')
+                class_name = new_obj[0]
+                """new_obj is equal to key.split('.')[0]
+                    this will split key, taking first key part"""
+                self.new(eval("{}".format(class_name))(**value))
+                """the if statement creates a new object with class name of the new_obj and value"""
+        except FileNotFoundError:
+            pass
