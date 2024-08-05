815c47d38b7ecb2850b31c194b4617c5e443d40f
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-05 22:07:20 +0300

    Hbnbclone
    Aibnb console

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-"""It defines interpreter location"""
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
 
-if __name__ == "__main__":
-    storage = FileStorage()
-    storage.reload()
+"""__init__ magic method to initialize package"""
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
